2ca3bcc19e40b3a249a1674df6094a2ae2e4b52f
Yashwant kaushal <54852557+yashwantkaushal@users.noreply.github.com>
2020-08-23 22:38:47 +0530

    Add files via upload

---
+++ NestedForLoop.java
@@ -0,0 +1,15 @@
+package loops;
+
+public class NestedForLoop {
+
+	public static void main(String[] args) {
+		
+		for(int j=1; j<=50; j++) {
+		   for(int i=1; i<=10;i++) {
+			   System.out.print(i +" ");
+		   }
+		   System.out.println();
+		}
+	}
+
+}
